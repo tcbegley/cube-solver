782b300e87cf99b1cab6008fbaef24988db446e4
tcbegley <tom@asidatascience.com>
2018-03-04 11:26:57 +0000

    Make colors dictionary two way lookup.

--- color.py
+++ color.py
@@ -4,4 +4,8 @@ We enumerate the colors and provide a dictionary for lookup of each color.
 
 U, R, F, D, L, B = range(6)
 
-COLORS = dict(zip(("U", "R", "F", "D", "L", "B"), range(6)))
+# Create two way lookup of strings / indices
+COLORS = {
+    **dict(zip(("U", "R", "F", "D", "L", "B"), range(6))),
+    **dict(zip(range(6), ("U", "R", "F", "D", "L", "B")))
+}
