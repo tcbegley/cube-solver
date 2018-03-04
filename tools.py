784ca9bec20cfd5fbbf72f6d22a5f4acf172b1c6
tcbegley <tom@asidatascience.com>
2018-03-04 10:59:11 +0000

    Initial commit of tools.py

---
+++ tools.py
@@ -0,0 +1,37 @@
+# Module for helpful miscellaneous shizzle
+import face_cube
+import cubie_cube
+import coord_cube
+import random
+import color
+
+
+# Verify string input
+def verify(s):
+    count = [0]*6
+    try:
+        for char in s:
+            count[color.c.index(char)] += 1
+    except (IndexError, ValueError):
+        return -1
+    for i in range(6):
+        if count[i] != 9:
+            return -1
+
+    fc = face_cube.FaceCube(s)
+    cc = fc.to_cubiecube()
+
+    return cc.verify()
+
+
+def random_cube():
+    cc = cubie_cube.CubieCube()
+    cc.set_flip(random.randint(0, coord_cube.flip))
+    cc.set_twist(random.randint(0, coord_cube.twist))
+    while True:
+        cc.set_corner(random.randint(0, coord_cube.corner))
+        cc.set_edge(random.randint(0, coord_cube.edge))
+        if cc.edge_parity() == cc.corner_parity():
+            break
+    fc = cc.to_facecube()
+    return fc.to_string()
