433d9457670cf15fd4e2af1108c46bc45a202928
tcbegley <tomcbegley@gmail.com>
2020-05-29 08:40:08 +0100

    Fix random cube generator

--- twophase/random.py
+++ twophase/random.py
@@ -1,16 +1,17 @@
 import random
 
-from . import coord_cube, cubie_cube
+from .cubes import cubiecube
+from .tables import Tables
 
 
 def random_cube():
-    cc = cubie_cube.CubieCube()
-    cc.set_flip(random.randint(0, coord_cube.flip))
-    cc.set_twist(random.randint(0, coord_cube.twist))
+    cc = cubiecube.CubieCube()
+    cc.flip = random.randint(0, Tables.FLIP)
+    cc.twist = random.randint(0, Tables.TWIST)
     while True:
-        cc.set_corner(random.randint(0, coord_cube.corner))
-        cc.set_edge(random.randint(0, coord_cube.edge))
-        if cc.edge_parity() == cc.corner_parity():
+        cc.corner = random.randint(0, Tables.CORNER)
+        cc.edge = random.randint(0, Tables.EDGE)
+        if cc.edge_parity == cc.corner_parity:
             break
     fc = cc.to_facecube()
     return fc.to_string()
